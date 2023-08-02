a12600a667797fa8525adb2279dfbf51d8f7a6a9
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-08-02 16:59:54 +0300

    update-2

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+const menuEl = document.querySelector('.js-link');
+console.log(menuEl)
+
+menuEl.textContent = 'О нас'
+
+const menuTl2 =document.querySelectorAll('.menu-link')
+console.log(menuTl2)
