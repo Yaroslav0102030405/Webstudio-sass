feab1f3326ac481e3f52dff70466e09ec7ba6bdb
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-08-02 17:24:01 +0300

    update-36

--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,7 @@
 const menuEl = document.querySelector('.js-link');
-console.log(menuEl)
+console.log(menuEl);
 
-menuEl.textContent = 'О нас'
+menuEl.textContent = 'О нас';
 
-const menuTl2 =document.querySelectorAll('.menu-link')
-console.log(menuTl2)
+const menuTl2 = document.querySelectorAll('.menu-link');
+console.log(menuTl2);
