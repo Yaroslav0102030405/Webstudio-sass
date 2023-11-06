6cba045d5cf09155a2feba8fe9e49d90505a703f
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-11-06 18:19:22 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,20 +2,21 @@
 
 $_POST = json_decode(file_get_contents('php:input'), true);
 
-$name = S_POST['name'];
-$phone = S_POST['phone'];
-$email = S_POST['email'];
-$comment = S_POST['comment'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
 
 
 $to = "lukovetskyi@gmail.com"; //кому отправлять письмо
-$subject = 'Заявка с сайта Webstudio'; // тема письма
-$message = "Имя: $name,Номер: $phone, Емаил: $email, Сообщение: $comment"; // само Сообщение
+$subject = 'Заявка с сайта webstudio'; // тема письма
+$message = "Имя: $name, Номер: $phone, Емаил: $email, Сообщение: $comment"; // само Сообщение
 
 $success = mail($to,$subject,$message);
 
 if(!$success) {
-    scho "Щшибка при выполнении запроса."
-} esle {
-    scho "Заявка успешно отправлена!"
+    echo "Ошибка при выполнении запроса."
+} 
+esle {
+    echo "Заявка успешно отправлена!"
 }
